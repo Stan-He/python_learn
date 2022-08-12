54123e17a514b36652932e9f9e1b955a679f56c3
meng.he <mengh.1988@gmail.com>
2022-08-12 14:53:29 +0800

    modify RLock case

--- learn_threading/thread_learn_RLock.py
+++ learn_threading/thread_learn_RLock.py
@@ -4,25 +4,29 @@ import time
 
 rlock=threading.RLock()
 data=0
+"""
+RLock使用了“拥有的线程”和“递归等级”的概念，处于锁定状态时，RLock被某个线程拥有。拥有RLock的线程可以再次调用acquire()，释放锁时需要调用release()相同次数。
+可以认为RLock包含一个锁定池和一个初始值为0的计数器，每次成功调用 acquire()/release()，计数器将+1/-1，为0时锁处于未锁定状态。
+"""
 
 def func():
-    print '%s acquire lock...' % threading.currentThread().getName()
+    print (f'{threading.current_thread().name} acquire lock...')
     if rlock.acquire():
-        print '%s get lock...' % threading.currentThread().getName()
+        print (f'{threading.current_thread().name} get lock...')
         time.sleep(2)
         
-        print '%s acquire lock again...' % threading.currentThread().getName()
+        print (f'{threading.current_thread().name} acquire lock again...')
         if rlock.acquire():
-            print '%s get lock...' % threading.currentThread().getName()
+            print (f'{threading.current_thread().name} get lock again...')
             global data
             data+=1
-            print data
+            print(data)
             time.sleep(2)
         
-        print '%s release lock...' % threading.currentThread().getName()
+        print (f'{threading.current_thread().name} release...')
         rlock.release()
         time.sleep(2)
-        print '%s release lock...' % threading.currentThread().getName()
+        print (f'{threading.current_thread().name} release...')
         rlock.release()
         
 t1=threading.Thread(target=func)
